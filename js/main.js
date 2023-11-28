958fce5ed30573a6bcde632356c9abc87fae4c84
OleksiiPro1 <ukr.top@ukr.net>
2023-11-28 19:06:35 +0100

    several tipes of using jquery with the p tag

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
+
+// $(document).ready(function(){
+//    // code
+// });
+
+
+// $(function(){
+//    // code
+// });
+
+// $(function(){
+//    var tagP = $('.mainText p').text('New text for the main page');
+//    // alert(tagP);
+// });
+
 $(function(){
-   $(':submit').click(function(event) {
-      $('.text2').attr('disabled','disabled');
-      event.preventDefault();
-   });
-   $(':reset').click(function(event) {
-      $('.text2').removeAttr('disabled');
-      event.preventDefault();
-   });
+   var tagP = $('.mainText p').hide(3000);
+   var tagP = $('.mainText p').show(3000);
+   // alert(tagP);
 });
+
+// $(function(){
+//    $(':submit').click(function(event) {
+//       $('.text2').attr('disabled','disabled');
+//       event.preventDefault();
+//    });
+//    $(':reset').click(function(event) {
+//       $('.text2').removeAttr('disabled');
+//       event.preventDefault();
+//    });
+// });
